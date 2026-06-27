acd3689b0434b8da1cf961561acc00070560f653
Didarul Alam <didarulalamrafi1312@gmail.com>
2026-06-27 13:25:17 +0600

    Logo Updated

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -1,22 +1,21 @@
-import Link from 'next/link';
-import React from 'react';
-import { IoHome } from 'react-icons/io5';
+import Link from "next/link";
+import React from "react";
+import { FcHome } from "react-icons/fc";
+import { IoHome } from "react-icons/io5";
 
 const Logo = () => {
-    return (
-        <div>
-            {/* LEFT */}
-            <Link href="/" className="flex items-center gap-3">
-                <div className="p-2 rounded-full bg-cyan-500/10 border border-cyan-300/30">
-                    <IoHome size={26} className="text-cyan-500" />
-                </div>
-
-                <h1 className="text-3xl font-bold tracking-wide">
-                    Property<span className='text-cyan-400 italic'>Hub</span>
-                </h1>
-            </Link>
+  return (
+    <div>
+      {/* LEFT */}
+      <Link href="/" className="flex items-center gap-3">
+        <div className="p-2 rounded-full bg-pink-500/10 border border-pink-300/30">
+          <FcHome size={26} className="text-pink-500" />
         </div>
-    );
+
+        <h1 className="text-3xl font-bold tracking-wide">PrimeHaven</h1>
+      </Link>
+    </div>
+  );
 };
 
 export default Logo;
